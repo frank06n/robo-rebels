35174c6ab61d8492591cfccbc672a45117d12259
unknown <dev.code38@gmail.com>
2025-04-19 10:45:40 +0530

    added bluetooth support

--- test_day1/test_day1.ino
+++ test_day1/test_day1.ino
@@ -1,19 +1,25 @@
 #include "Wire.h"
 #include <MPU6050_light.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial HC05(3, 2);  // RX, TX
+
+char command;
 
 MPU6050 mpu(Wire);
 unsigned long previous_time = 0;  // Timer to calculate delta time
 
 // PID Constants (Tune these for best performance)
-float Kp = 100;
-float Ki = 15;
-float Kd = 0.0;
+float Kp = 150;
+float Ki = 20;
+float Kd = 0.01;
 
 float setpoint = 0;  // Desired pitch angle (robot balanced at 0°)
 float previous_error = 0.0;
 float integral = 0.0;
 
-float forward_offset = 0.8; // Tilt target forward by _°, adjust for speed
+float forward_offset = 0.0; // Tilt target forward by _°, adjust for speed
+int forward_offset_multiplier = 0;
 
 // Motor Driver Pins (L298)
 #define ENA 9  // Left Motor Speed (PWM)
@@ -27,6 +33,7 @@ void setup() {
   Serial.begin(9600);
   Wire.begin();
   Wire.setClock(400000);
+  HC05.begin(9600);  //Set the baud rate to your Bluetooth module.
   
   byte status = mpu.begin();
   Serial.print(F("MPU6050 status: "));
@@ -97,12 +104,70 @@ void loop() {
 // Serial.print("  Z: ");
 // Serial.println(mpu.getAngleZ());
 
+if (HC05.available() > 0) {
+    command = HC05.read();
+
+    // Stop(); //initialize with motors stoped
+
+    Serial.print("Received: ");
+    Serial.println(command);
+
+    
+      switch (command) {
+        case 'F':
+          forward_offset_multiplier = 1;
+          break;
+        case 'B':
+          forward_offset_multiplier = -1;
+          break;
+        case '0':
+          forward_offset = 0;
+          break;
+        case '1':
+          forward_offset = 0.2;
+          break;
+        case '2':
+          forward_offset = 0.5;
+          break;
+        case '3':
+          forward_offset = 0.9;
+          break;
+        case '4':
+          forward_offset = 1.2;
+          break;
+        case '5':
+          forward_offset = 1.7;
+          break;
+        case '6':
+          forward_offset = 2.0;
+          break;
+        case '7':
+          forward_offset = 2.3;
+          break;
+        case '8':
+          forward_offset = 2.6;
+          break;
+        case '9':
+          forward_offset = 2.8;
+          break;
+        case 'q':
+          forward_offset = 3.0;
+          break;
+        case 'S':
+          forward_offset_multiplier = 0;
+          break;
+      }
+      Serial.print('Mult: ');
+      Serial.print(forward_offset_multiplier);
+      Serial.print(', offs: ');
+      Serial.println(forward_offset);
+}
 
   if (dt >= 0.01) {  // Run every 10ms
 
     float pitch = mpu.getAngleY();  // Use Y-axis as pitch
 
-     float target = setpoint + forward_offset;
+     float target = setpoint + forward_offset_multiplier * forward_offset;
 
     // Calculate PID
     float error = target - pitch;
